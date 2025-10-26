64c5a54fe150d4da509d39495b0aa338c714d93d
Kazeem Asifat <asifatkazeem@yahoo.com>
2025-10-26 16:10:47 +0100

    Changed the template engine to ejs

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const { engine: hbsEngine } = require('express-handlebars');
+//const { engine: hbsEngine } = require('express-handlebars');
 
-const rootDir = require('./util/path');
+const errorController = require('./controllers/error');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const errorController = require('./controllers/error');
 
 const app = express();
 
 // Set the template engine to what we want to use (express-handlebars v6+)
-app.engine('hbs', hbsEngine({
+/*app.engine('hbs', hbsEngine({
   extname: '.hbs',
   defaultLayout: 'main-layout',
   layoutsDir: path.join(rootDir, 'views', 'layouts')
 }));
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs');*/
+app.set('view engine', 'ejs');
 app.set('views', 'views'); // Default value is 'views', so this line is optional
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(rootDir, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use('/shop', shopRoutes);
--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,10 @@
 const ProductModel = require('../models/product');
+const path = require('../util/path');
 
 exports.createProductPage = (req, res, next) => {
   res.render('add-product', { 
     pageTitle: 'Add Product - Admin',
+    path: '/admin/add-product',
     formsCSS: true,
     productCSS: true,
     activeAddProduct: true,
@@ -20,6 +22,7 @@ exports.getProducts = (req, res, next) => {
   ProductModel.fetchAll(products => {
     res.render('shop', {
       prods: products,
+      path: '/shop',
       hasProducts: products.length > 0,
       pageTitle: 'All Products - Shop',
       activeShop: true,
